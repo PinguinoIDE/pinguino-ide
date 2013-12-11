d29bd9d89a9bbd4fd543ec6e28b7362ce7b0c6c4
Yeison Cardona <yeison.eng@gmail.com>
2013-12-10 23:59:07 -0500

    Switch absolute path to regular path of Home dir.

--- qtgui/ide/helpers/constants.py
+++ qtgui/ide/helpers/constants.py
@@ -11,7 +11,7 @@ from PySide.QtCore import QDir
 IDE_HOME_DIR = sys.path[0]
 
 PINGUINO_EXAMPLES_DIR = os.path.join(IDE_HOME_DIR, "pinguino", "examples")
-HOME_DIR = QDir.home().absolutePath()
+HOME_DIR = QDir.home().path()
 DESKTOP_DIR = QDir.AllDirs
 
 
@@ -22,6 +22,7 @@ PINGUINO_STDOUT_FILE = os.path.join(IDE_HOME_DIR, "pinguino", "source", "stdout"
 IDE_STDOUT_FILE = os.path.join(IDE_HOME_DIR, "pinguino.out")
 IDE_STDERR_FILE = os.path.join(IDE_HOME_DIR, "pinguino.err")
 IDE_CONFIG_FILE = os.path.join(IDE_HOME_DIR, "qtgui", "config", "pinguino.conf")
+IDE_NAMESPACES_FILE = os.path.join(IDE_HOME_DIR, "qtgui", "config", "namespaces.pickle")
 
 NAME = "Pinguino IDE"
 VERSION = "11.0"
