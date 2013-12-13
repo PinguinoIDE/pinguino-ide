14f4d8bc8400f9b2029007cb3b12f566d209a5ba
Yeison Cardona <yeison.eng@gmail.com>
2013-12-12 20:35:18 -0500

    Recompile frames.

--- qtgui/frames/stdout.py
+++ qtgui/frames/stdout.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/yeison/Documentos/Pinguino/pinguino-ide/qtgui/frames/stdout.ui'
 #
-# Created: Tue Dec 10 23:44:16 2013
+# Created: Thu Dec 12 20:11:09 2013
 #      by: pyside-uic 0.2.14 running on PySide 1.1.2
 #
 # WARNING! All changes made in this file will be lost!
