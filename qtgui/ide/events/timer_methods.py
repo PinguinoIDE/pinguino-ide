547d072e79ba83e28b6ee54e9149ac1c8c607bba
Yeison Cardona <yeison.eng@gmail.com>
2013-12-17 14:50:32 -0500

    Add update autocompleter for variables, directives and functions defined.

--- qtgui/ide/events/timer_methods.py
+++ qtgui/ide/events/timer_methods.py
@@ -7,6 +7,8 @@ from ..helpers.decorators import Decorator
 from ..tools.code_navigator import CodeNavigator
 from ..tools.files import Files
 
+from ..code_editor.autocomplete_icons import CompleteIcons
+
 ########################################################################
 class TimerMethods(object):
     """"""
@@ -26,10 +28,12 @@ class TimerMethods(object):
         
         index = 0
         self.main.tableWidget_functions.setRowCount(len(functions_parse))
+        self.completer_funtions = []
         for funtion in functions_parse:
             item = QtGui.QTableWidgetItem()
             self.main.tableWidget_functions.setVerticalHeaderItem(index, item) 
             item.setText(funtion["name"])
+            self.completer_funtions.append(funtion["name"])
             
             self.main.tableWidget_functions.setItem(index, 0, QtGui.QTableWidgetItem())
             self.main.tableWidget_functions.setItem(index, 1, QtGui.QTableWidgetItem())
@@ -58,10 +62,12 @@ class TimerMethods(object):
         
         index = 0
         self.main.tableWidget_directives.setRowCount(len(directives_parse))
+        self.completer_directives = []
         for directive in directives_parse:
             item = QtGui.QTableWidgetItem()
             self.main.tableWidget_directives.setVerticalHeaderItem(index, item)
             item.setText(directive["name"])
+            self.completer_directives.append(directive["name"])
             
             self.main.tableWidget_directives.setItem(index, 0, QtGui.QTableWidgetItem())
             self.main.tableWidget_directives.setItem(index, 1, QtGui.QTableWidgetItem())
@@ -90,10 +96,12 @@ class TimerMethods(object):
         
         index = 0
         self.main.tableWidget_variables.setRowCount(len(variables_parse))
+        self.completer_variables = []
         for variable in variables_parse:
             item = QtGui.QTableWidgetItem()
             self.main.tableWidget_variables.setVerticalHeaderItem(index, item)
             item.setText(variable["name"])
+            self.completer_variables.append([variable["name"], variable["type"]])
             
             self.main.tableWidget_variables.setItem(index, 0, QtGui.QTableWidgetItem())
             self.main.tableWidget_variables.setItem(index, 1, QtGui.QTableWidgetItem())
@@ -104,3 +112,56 @@ class TimerMethods(object):
             self.main.tableWidget_variables.item(index, 1).setText(variable["line"])
             index += 1
             
+            
+    #----------------------------------------------------------------------
+    @Decorator.timer(1000)
+    @Decorator.requiere_main_focus()
+    @Decorator.requiere_text_mode()
+    @Decorator.requiere_open_files()
+    def update_autocompleter(self):
+        
+        editor = self.main.tabWidget_files.currentWidget()
+        
+        if not getattr(self, "completer_funtions", False):
+            self.completer_funtions = []
+            functions_parse = CodeNavigator.get_functions(editor)
+            for funtion in functions_parse:
+                self.completer_funtions.append(funtion["name"])
+                
+        if not getattr(self, "completer_variables", False):
+            self.completer_variables = []
+            variables_parse = CodeNavigator.get_variables(editor)
+            for variable in variables_parse:
+                self.completer_variables.append([variable["name"], variable["type"]])
+                
+        if not getattr(self, "completer_directives", False):
+            self.completer_directives = []
+            directives_parse = CodeNavigator.get_directives(editor)
+            for directive in directives_parse:
+                self.completer_directives.append(directive["name"])
+            
+        
+        editor.text_edit.completer.removeTemporalItems("directives")
+        editor.text_edit.completer.removeTemporalItems("variables")
+        editor.text_edit.completer.removeTemporalItems("functions")
+        
+        for item in self.completer_directives:
+            editor.text_edit.completer.addTemporalItem("directives", item, self.ICONS.iconDirectives)
+        
+        for item in self.completer_funtions:
+            editor.text_edit.completer.addTemporalItem("functions", item, self.ICONS.iconFunction)
+            
+        for item, type_ in self.completer_variables:
+            type_ = type_.split()[-1]
+            if type_ in "int float bool byte bool double struct union".split():
+                icon = getattr(self.ICONS, "icon"+type_.capitalize())
+            else:
+                icon = None
+            
+            editor.text_edit.completer.addTemporalItem("variables", item, icon)
+            
+            
+
+        self.completer_funtions = []
+        self.completer_directives = []
+        self.completer_variables = []
