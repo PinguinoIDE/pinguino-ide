21646ab02967c1ebf60cb1dcc50b6fbd62448b5e
Yeison Cardona <yeison.eng@gmail.com>
2013-12-17 10:55:28 -0500

    Recompile blocks.

--- qtgui/gide/bloques/inside/inside2_bool.py
+++ qtgui/gide/bloques/inside/inside2_bool.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/yeison/Documentos/Pinguino/pinguino-ide/qtgui/gide/bloques/inside/inside2_bool.ui'
 #
-# Created: Thu Dec 12 20:11:10 2013
+# Created: Mon Dec 16 10:09:12 2013
 #      by: pyside-uic 0.2.14 running on PySide 1.1.2
 #
 # WARNING! All changes made in this file will be lost!
