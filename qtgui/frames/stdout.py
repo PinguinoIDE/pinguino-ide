319c2d9adede4eb7f87ff99b5bc1644b21d5f470
Yeison Cardona <yeison.eng@gmail.com>
2013-12-17 14:52:04 -0500

    Recompile Qt4 .ui and .qrc files.

--- qtgui/frames/stdout.py
+++ qtgui/frames/stdout.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/yeison/Documentos/Pinguino/pinguino-ide/qtgui/frames/stdout.ui'
 #
-# Created: Tue Dec 17 11:06:29 2013
+# Created: Tue Dec 17 14:38:13 2013
 #      by: pyside-uic 0.2.14 running on PySide 1.1.2
 #
 # WARNING! All changes made in this file will be lost!
