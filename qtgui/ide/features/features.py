d1049d838a3e295325e0a7f383b6612cf3462438
Yeison Cardona <yeison.eng@gmail.com>
2013-12-17 19:32:03 -0500

    Add switch for autocomplete feature.

--- qtgui/ide/features/features.py
+++ qtgui/ide/features/features.py
@@ -14,6 +14,15 @@ class PinguinoFeatures(PinguinoEvents):
         self.PinguinoKIT = GraphicalIDE(self)
         self.__preconfig_kit__()
         
+        self.main.actionAutocomplete.setChecked(self.configIDE.config("Features", "autocomplete", True))
+        
+        self.main.actionSwitch_ide.setChecked(self.configIDE.config("Features", "graphical", False))
+        self.switch_ide_mode(self.configIDE.config("Features", "graphical", False))
+        
+        self.recent_files = self.configIDE.get_recents()
+        self.update_recents_menu()
+        
+        
     #----------------------------------------------------------------------
     def __preconfig_kit__(self):
         """"""
@@ -21,18 +30,23 @@ class PinguinoFeatures(PinguinoEvents):
         self.main.dockWidget_blocks.setVisible(False)        
         
     #----------------------------------------------------------------------
-    def switch_ide_mode(self, toogle):
+    def switch_ide_mode(self, toggle):
         """"""
-        self.main.tabWidget_graphical.setVisible(toogle and self.main.tabWidget_graphical.count()>0)
-        self.main.tabWidget_files.setVisible(not toogle and self.main.tabWidget_files.count()>0)
+        self.main.actionSwitch_ide.setChecked(toggle)
+        self.main.tabWidget_graphical.setVisible(toggle and self.main.tabWidget_graphical.count()>0)
+        self.main.tabWidget_files.setVisible(not toggle and self.main.tabWidget_files.count()>0)
         
-        self.main.dockWidget_blocks.setVisible(toogle)  
-        self.main.dockWidget_tools.setVisible(not toogle)  
-        self.main.toolBar_search_replace.setVisible(not toogle)
-        self.main.toolBar_edit.setVisible(not toogle)
-        self.main.toolBar_graphical.setVisible(toogle)
+        self.main.dockWidget_blocks.setVisible(toggle)  
+        self.main.dockWidget_tools.setVisible(not toggle)  
+        self.main.toolBar_search_replace.setVisible(not toggle)
+        self.main.toolBar_edit.setVisible(not toggle)
+        self.main.toolBar_graphical.setVisible(toggle)
         
-        self.main.actionView_Pinguino_code.setEnabled(toogle)
+        self.main.actionView_Pinguino_code.setEnabled(toggle)
+        
+        
+        self.configIDE.set("Features", "graphical", toggle)
+        self.configIDE.save_config()         
         
         self.tab_changed()
         
@@ -48,3 +62,16 @@ class PinguinoFeatures(PinguinoEvents):
         editor = tab.currentWidget()
         if editor is None: return False
         return getattr(editor, "is_widget", False)
+    
+    #----------------------------------------------------------------------
+    def is_autocomplete_enable(self):
+        """"""
+        return self.main.actionAutocomplete.isChecked()
+    
+    #----------------------------------------------------------------------
+    def switch_autocomplete(self):
+        """"""
+        enable = self.main.actionAutocomplete.isChecked()
+        self.configIDE.set("Features", "autocomplete", enable)
+        self.configIDE.save_config()        
+        
