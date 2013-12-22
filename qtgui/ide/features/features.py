4e42434e7f45dccfd212b3a072a5171aa78d2d80
Yeison Cardona <yeison.eng@gmail.com>
2013-12-22 10:20:09 -0500

    Add self to python shell.

--- qtgui/ide/features/features.py
+++ qtgui/ide/features/features.py
@@ -26,6 +26,7 @@ class PinguinoFeatures(PinguinoEvents):
         
         #self.main.dockWidget_output.add
         self.main.plainTextEdit_output = PinguinoTerminal(self.main.dockWidgetContents_2)
+        self.main.plainTextEdit_output.shell.statement_module.pinguino_main = self
         self.main.gridLayout_3.addWidget(self.main.plainTextEdit_output, 0, 0, 1, 1)
         
         
