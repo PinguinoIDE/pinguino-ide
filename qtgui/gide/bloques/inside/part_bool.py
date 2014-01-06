741b608050c8bc94cda834019400b756d9acf31e
YeisonEng <yeison.eng@gmail.com>
2014-01-06 18:35:05 -0500

    Recompile blocks.

--- qtgui/gide/bloques/inside/part_bool.py
+++ qtgui/gide/bloques/inside/part_bool.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/yeison/.virtualenvs/pinguino_env/pinguino/pinguino-ide/qtgui/gide/bloques/inside/part_bool.ui'
 #
-# Created: Sat Jan  4 20:01:49 2014
+# Created: Mon Jan  6 18:01:11 2014
 #      by: pyside-uic 0.2.14 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
--- qtgui/gide/bloques/nested_in_code_next/nested_second.py
+++ qtgui/gide/bloques/nested_in_code_next/nested_second.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/yeison/.virtualenvs/pinguino_env/pinguino/pinguino-ide/qtgui/gide/bloques/nested_in_code_next/nested_second.ui'
 #
-# Created: Sat Jan  4 20:01:48 2014
+# Created: Mon Jan  6 18:01:12 2014
 #      by: pyside-uic 0.2.14 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
