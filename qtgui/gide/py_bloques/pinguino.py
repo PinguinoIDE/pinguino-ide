6c05faa5e023b7ccfadc9d306943cf09448dbb0d
YeisonEng <yeison.eng@gmail.com>
2014-03-01 08:57:27 -0500

    Updated LCD blocks, added millis().

--- qtgui/gide/py_bloques/pinguino.py
+++ qtgui/gide/py_bloques/pinguino.py
@@ -8,23 +8,24 @@ Kit = Kit()
 
 ########################################################################
 class Pinguino(object):
-    
+
     pinMode = lambda n:Kit.addFunct("Pinguino", "pinMode", 2, fill=["spin-int", "choice-io"])
     delay = lambda n:Kit.addFunct("Pinguino", "delay", 1, fill=["spin-int"])
     delayMicroseconds = lambda n:Kit.addFunct("Pinguino", "delayMicroseconds", 1, fill=["spin-int"])
     toggle = lambda n:Kit.addFunct("Pinguino", "toggle", 1, fill=["spin-int"])
-    
+    millis = lambda n:Kit.addFunct("Pinguino", "millis", 0)
+
     digitalWrite = lambda n:Kit.addFunct("Pinguino", "digitalWrite", 2, fill=["spin-int", "choice-hl"])
     analogWrite = lambda n:Kit.addFunct("Pinguino", "analogWrite", 2, fill=["spin-int", "slider"])
     digitalRead = lambda n:Kit.addFunct("Pinguino", "digitalRead", 1, fill=["spin-int"])
     analogRead = lambda n:Kit.addFunct("Pinguino", "analogRead", 1, fill=["spin-int"])
-    
+
     digitalRead_o = lambda n:Kit.addFunct("Pinguino", "digitalRead", 1, t="output", fill=["spin-int"])
     analogRead_o = lambda n:Kit.addFunct("Pinguino", "analogRead", 1, t="output", fill=["spin-int"])
-    
+
     digitalRead_b = lambda n:Kit.addFunct("Pinguino", "digitalRead", 1, t="output-bool", fill=["spin-int"])
     analogRead_b = lambda n:Kit.addFunct("Pinguino", "analogRead", 1, t="output-bool", fill=["spin-int"])
-    
+
     #----------------------------------------------------------------------
     def setup(self):
         st = Block.Function("Pinguino")
@@ -37,7 +38,7 @@ class Pinguino(object):
         st = Block.Function("Pinguino")
         st.addLabel("loop")
         st.addSyntax("()")
-        return st.getBlock()        
+        return st.getBlock()
 
     #----------------------------------------------------------------------
     def define(self):
@@ -57,7 +58,7 @@ class Pinguino(object):
         st.addLabel("global")
         st.addSyntax(" variables")
         return st.getBlock()
-    
+
 
     #----------------------------------------------------------------------
     def types(self):
@@ -66,4 +67,4 @@ class Pinguino(object):
         st.addSyntax(" ")
         st.addEdit("Pinguino")
         return st.getBlock()
-    
+
