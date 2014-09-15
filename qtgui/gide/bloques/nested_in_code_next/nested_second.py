2f20888274158bcaba181908d1c0314f7dea4eec
YeisonEng <yeison.eng@gmail.com>
2014-09-14 23:00:58 -0500

    Improved "Wiki documentation" viewer.

--- qtgui/gide/bloques/nested_in_code_next/nested_second.py
+++ qtgui/gide/bloques/nested_in_code_next/nested_second.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file '/home/yeison/Documentos/python/dev/developing/pinguino/pinguino-ide/qtgui/gide/bloques/nested_in_code_next/nested_second.ui'
 #
-# Created: Fri Sep  5 23:23:26 2014
-#      by: pyside-uic 0.2.14 running on PySide 1.2.1
+# Created: Sun Sep 14 22:40:37 2014
+#      by: pyside-uic 0.2.14 running on PySide 1.2.2
 #
 # WARNING! All changes made in this file will be lost!
 
