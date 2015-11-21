f3937c22d668bb3a64cb5d1bc23e2f2cc9fac7d0
YeisonEng <yeison.eng@gmail.com>
2015-11-20 19:30:53 -0500

    fixed installer for windows

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+import os
 from setuptools import setup, find_packages
+
+if os.name == "posix": #GNU/Linux
+    os_kwargs = {"data_files": [
+                                ("/usr/share/applications", ["pinguino12.desktop"]),
+                                ("/usr/share/icons/hicolor/256x256", ["pinguino/qtgui/resources/art/pinguino_logo-256x256.png"]),
+                                ("/usr/share/icons/hicolor/256x256", ["pinguino/qtgui/resources/art/pinguino_logo_background_blue-256x256.png"]),
+                                ],
+                }
+
+elif os.name == "os2":  #Mac OS X
+    os_kwargs = {}
+
+elif os.name == "nt":  #Windows
+    os_kwargs = {"data_files": [
+                                ("C:\pinguino", ["pinguino12.desktop"]),
+                                ("C:\pinguino\icons", ["pinguino/qtgui/resources/art/pinguino_logo-256x256.ico"]),
+                                ("C:\pinguino\icons", ["pinguino/qtgui/resources/art/pinguino_logo_background_blue-256x256.ico"]),
+                                ],
+                }
 
 
 setup(name = "pinguino",
-      version = "12.0.12",
+      version = "12.0.16",
       description = "Open Hardware Electronics Prototyping Platform, Open Source Integrated Development Environment (IDE)",
       description_file = "README.rst",
 
@@ -18,8 +38,7 @@ setup(name = "pinguino",
       download_url = "",
 
       license = "GPLv2",
-      install_requires = ["gitpython", "hgapi", "pyusb==1.0.0b2"],
-      keywords = 'microchip, electronic, prototyping, IDE',
+      keywords = "microchip, electronic, prototyping, IDE",
 
       classifiers=[#list of classifiers in https://pypi.python.org/pypi?:action=list_classifiers
                    "Environment :: X11 Applications :: Qt",
@@ -29,19 +48,26 @@ setup(name = "pinguino",
                    "Programming Language :: Python",
                    ],
 
-      scripts = ["bin/pinguino",
-                 "bin/pinguino-cmd",
-                 "bin/pinguino-reset",
-                 ],
-
       packages = find_packages(),
-
       include_package_data = True,
 
-      data_files=[
-          ('/usr/share/applications', ['pinguino12.desktop']),
-          ('/usr/share/icons/hicolor/256x256', ['pinguino/qtgui/resources/art/pinguino_logo-256x256.png']),
-          ('/usr/share/icons/hicolor/256x256', ['pinguino/qtgui/resources/art/pinguino_logo_background_blue-256x256.png']),
-          ],
+      install_requires = [
+                          "gitpython",
+                          "hgapi",
+                          "pyusb==1.0.0b2",
+                          "pyside",
+                          ],
 
+      scripts = [
+                 "cmd/pinguinoide",			# pinguino ide launcher
+                 "cmd/pinguinoide.pyw",		# pinguino ide launcher with extension for windows
+                 "cmd/pinguino-cmd",		# pinguino command line
+                 "cmd/pinguino-cmd.py",		# pinguino command line with extension for windows
+                 "cmd/pinguino-reset",
+                 "cmd/pinguino-reset.py",
+                 ],
+
+      zip_safe = False,
+
+      **os_kwargs
       )
