684313955645d014763866e4be5e81cfe25c8003
YeisonEng <yeison.eng@gmail.com>
2015-11-26 17:56:18 -0500

    future pinguino libraries developer tools

--- pinguino/dev.py
+++ pinguino/dev.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python
-#-*- coding: utf-8 -*-
-
-#----------------------------------------------------------------------
-def get_libraries_dir():
-    """"""
+#-*- coding: utf-8 -*
 
 
-#----------------------------------------------------------------------
-def post_install(self, lib_name):
-    """"""
-    lista = []
+# #----------------------------------------------------------------------
+# def get_libraries_dir():
+    # """"""
 
-    path_examples = os.path.join(os.getenv("PINGUINO_USERLIBS_PATH"), "usr", lib_name, "examples")
-    dest_examples = os.path.join(os.getenv("PINGUINO_USERLIBS_PATH"), "library_examples", lib_name)
-    lista.append([path_examples, dest_examples])
 
-    path_blocks = os.path.join(os.getenv("PINGUINO_USERLIBS_PATH"), "usr", lib_name, "blocks")
-    dest_blocks = os.path.join(os.getenv("PINGUINO_USERLIBS_PATH"), "blocks", lib_name)
-    lista.append([path_blocks, dest_blocks])
+# #----------------------------------------------------------------------
+# def post_install(self, lib_name):
+    # """"""
+    # lista = []
 
-    for src, dest in lista:
-        if os.path.exists(src):
-            if os.path.exists(dest): shutil.rmtree(dest)
-            shutil.copytree(src, dest)
+    # path_examples = os.path.join(os.getenv("PINGUINO_USERLIBS_PATH"), "usr", lib_name, "examples")
+    # dest_examples = os.path.join(os.getenv("PINGUINO_USERLIBS_PATH"), "library_examples", lib_name)
+    # lista.append([path_examples, dest_examples])
 
-    # self.pinguinoAPI.force_reload_libs()
-    # self.update_reserved_words()
-    # delattr(self, "assistant")
-    self.refresh_libraries()
+    # path_blocks = os.path.join(os.getenv("PINGUINO_USERLIBS_PATH"), "usr", lib_name, "blocks")
+    # dest_blocks = os.path.join(os.getenv("PINGUINO_USERLIBS_PATH"), "blocks", lib_name)
+    # lista.append([path_blocks, dest_blocks])
+
+    # for src, dest in lista:
+        # if os.path.exists(src):
+            # if os.path.exists(dest): shutil.rmtree(dest)
+            # shutil.copytree(src, dest)
+
+    # # self.pinguinoAPI.force_reload_libs()
+    # # self.update_reserved_words()
+    # # delattr(self, "assistant")
+    # self.refresh_libraries()
 
 
