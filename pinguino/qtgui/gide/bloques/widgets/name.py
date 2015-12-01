03c063555d9fbeeca85a90c114993d90975f4daa
YeisonEng <yeison.eng@gmail.com>
2015-11-30 23:33:46 -0500

    recompile frames

--- pinguino/qtgui/gide/bloques/widgets/name.py
+++ pinguino/qtgui/gide/bloques/widgets/name.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/yeison/Documentos/Desarrollo/Pinguino/GitHub/pinguino-ide/pinguino/qtgui/gide/bloques/widgets/name.ui'
 #
-# Created: Thu Nov 26 17:38:48 2015
+# Created: Mon Nov 30 23:27:47 2015
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
