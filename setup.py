a558b090485c760e2ecf85f4e9e15c7016777e7a
YeisonEng <yeison.eng@gmail.com>
2016-01-22 17:40:01 -0500

    Update release.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ elif os.name == "nt":  #Windows
 
 
 setup(name = "pinguino",
-      version = "12.0.26",
+      version = "12.0.27",
       description = "Open Hardware Electronics Prototyping Platform, Open Source Integrated Development Environment (IDE)",
       description_file = "README.rst",
 
