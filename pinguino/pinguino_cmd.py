7513685bf007a40f0b237af9d85fc82d055b8a66
YeisonEng <yeison.eng@gmail.com>
2016-01-25 15:18:33 -0500

    Update new bootloaders features.

--- pinguino/pinguino_cmd.py
+++ pinguino/pinguino_cmd.py
@@ -75,7 +75,8 @@ def build_argparse():
     #parser.add_argument("-8c", "--8bit_compiler", dest="compiler", nargs=1, default=None, help="8bit compiler: SDCC or XC8")
     parser.add_argument("--sdcc", dest="compiler_sdcc", action="store_true", default=None, help="SDCC for 8bit compiler")
     parser.add_argument("--xc8", dest="compiler_xc8", action="store_true", default=None, help="XC8 for 8bit compiler")
-    parser.add_argument("-l", "--boot", dest="bootloader", nargs=1, default=False, help="set bootloader option")
+    #parser.add_argument("-l", "--boot", dest="bootloader", nargs=1, default=False, help="set bootloader option")
+    parser.add_argument("--icsp", dest="icsp", action="store_true", default=False, help="use ICSP (default use bootloader)")
     parser.add_argument("-x", "--upload", dest="upload", action="store_true", default=False, help="upload code")
     parser.add_argument("-g", "--hex", dest="hex_file", action="store_true", default=False, help="print hex_file")
 
@@ -106,10 +107,18 @@ if parser.board:
     pinguino.set_board(parser.board)
     printb("using {} board".format(parser.board.name), BColors.Green)
 
-    if parser.bootloader:
-        bootloader = pinguino.dict_boot.get(parser.bootloader[0].lower(), parser.board.bldr)
-        pinguino.set_bootloader(*bootloader)
-    printb("using {} bootloader".format(pinguino.get_board().bldr), BColors.Green)
+    #if parser.bootloader:
+        #bootloader = pinguino.dict_boot.get(parser.bootloader[0].lower(), parser.board.bldr)
+        #pinguino.set_bootloader(*bootloader)
+        #printb("using {} bootloader".format(pinguino.get_board().bldr), BColors.Green)
+
+    if parser.icsp:
+        #do nothing
+        pinguino.set_icsp()  #use ICSP instead of bootloader
+        printb("ICSP mode used", BColors.Green)
+    else:
+        printb("Bootloader {} used".format(pinguino.get_board().bldr), BColors.Green)
+
 
     if parser.board.arch == 8:
         if parser.compiler_sdcc:
