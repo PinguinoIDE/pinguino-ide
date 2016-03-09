c512efb06ee95b81ea6dce494b2c48b538ca49ab
YeisonEng <yeison.eng@gmail.com>
2016-03-09 17:05:27 -0500

    Added feature to save custom paths and remember the last configurations.

--- pinguino/qtgui/ide/tools/paths.py
+++ pinguino/qtgui/ide/tools/paths.py
@@ -23,21 +23,13 @@ class Paths(object):
 
         self.load_paths()
 
-
-        #self.OPTIMIZATION = "-O2 -O3 -Os".split()
-        #self.main.comboBox_optimization.clear()
-        #self.main.comboBox_optimization.addItems(self.OPTIMIZATION)
-        #self.init_groups()
-
-
-        #self.connect(self.main.pushButton_paths_set_default, QtCore.SIGNAL("clicked()"), self.set_default_values)
-
-
         self.connect(self.main.lineEdit_path_sdcc_bin, QtCore.SIGNAL("editingFinished()"), self.save_paths)
         self.connect(self.main.lineEdit_path_gcc_bin, QtCore.SIGNAL("editingFinished()"), self.save_paths)
         self.connect(self.main.lineEdit_path_xc8_bin, QtCore.SIGNAL("editingFinished()"), self.save_paths)
         self.connect(self.main.lineEdit_path_8_libs, QtCore.SIGNAL("editingFinished()"), self.save_paths)
         self.connect(self.main.lineEdit_path_32_libs, QtCore.SIGNAL("editingFinished()"), self.save_paths)
+
+        self.connect(self.main.checkBox_paths_default, QtCore.SIGNAL("toggled(bool)"), self.set_default_values)
 
 
 
@@ -52,30 +44,42 @@ class Paths(object):
         p8libs = self.main.lineEdit_path_8_libs.text()
         p32libs = self.main.lineEdit_path_32_libs.text()
 
-        self.configIDE.set("Paths", "sdcc_bin", sdcc_bin)
-        self.configIDE.set("Paths", "gcc_bin", gcc_bin)
-        self.configIDE.set("Paths", "xc8_bin", xc8_bin)
-        self.configIDE.set("Paths", "pinguino_8_libs", p8libs)
-        self.configIDE.set("Paths", "pinguino_32_libs", p32libs)
+        #self.configIDE.set("Paths", "sdcc_bin", sdcc_bin)
+        #self.configIDE.set("Paths", "gcc_bin", gcc_bin)
+        #self.configIDE.set("Paths", "xc8_bin", xc8_bin)
+        #self.configIDE.set("Paths", "pinguino_8_libs", p8libs)
+        #self.configIDE.set("Paths", "pinguino_32_libs", p32libs)
+
+        self.configIDE.set("PathsCustom", "sdcc_bin", sdcc_bin)
+        self.configIDE.set("PathsCustom", "gcc_bin", gcc_bin)
+        self.configIDE.set("PathsCustom", "xc8_bin", xc8_bin)
+        self.configIDE.set("PathsCustom", "pinguino_8_libs", p8libs)
+        self.configIDE.set("PathsCustom", "pinguino_32_libs", p32libs)
 
         self.configIDE.save_config()
 
 
 
     #----------------------------------------------------------------------
-    def load_paths(self, config=None):
+    def load_paths(self, section=None):
         """"""
+        self.configIDE.load_config()
+        if section is None:
+            section = self.configIDE.config("Features", "pathstouse", "Paths")
 
-        if config is None:
-            self.configIDE.load_config()
-            config = self.configIDE
+        self.main.checkBox_paths_default.setChecked(section=="Paths")
 
-        sdcc_bin = config.get("Paths", "sdcc_bin")
-        gcc_bin = config.get("Paths", "gcc_bin")
-        xc8_bin = config.get("Paths", "xc8_bin")
-        p8libs = config.get("Paths", "pinguino_8_libs")
-        p32libs = config.get("Paths", "pinguino_32_libs")
+        def short(section, option):
+            if section == "Paths":
+                return self.configIDE.get(section, option)
+            elif section == "PathsCustom":
+                return self.configIDE.config(section, option, self.configIDE.get("Paths", option))
 
+        sdcc_bin = short(section, "sdcc_bin")
+        gcc_bin = short(section, "gcc_bin")
+        xc8_bin = short(section, "xc8_bin")
+        p8libs = short(section, "pinguino_8_libs")
+        p32libs = short(section, "pinguino_32_libs")
 
         self.main.lineEdit_path_sdcc_bin.setText(sdcc_bin)
         self.main.lineEdit_path_gcc_bin.setText(gcc_bin)
@@ -85,17 +89,20 @@ class Paths(object):
 
 
 
-    ##----------------------------------------------------------------------
-    #def set_default_values(self):
-        #""""""
-        #config_paths = RawConfigParser()
-        #config_paths.readfp(open(os.path.join(os.getenv("PINGUINO_LIB"), "qtgui", "config", "pinguino.{PINGUINO_OS_NAME}.conf".format(**os.environ)), "r"))
-        #self.load_paths(config_paths)
+    #----------------------------------------------------------------------
+    def set_default_values(self, default):
+        """"""
 
-        #self.save_paths()
+        self.configIDE.load_config()
 
+        if default:
+            self.load_paths(section="Paths")
+            self.configIDE.set("Features", "pathstouse", "Paths")
 
+        else:
+            self.load_paths(section="PathsCustom")
+            self.configIDE.set("Features", "pathstouse", "PathsCustom")
 
-
+        self.configIDE.save_config()
 
 
